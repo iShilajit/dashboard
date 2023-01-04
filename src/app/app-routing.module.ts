2c8849e3640a67e5964e32f129b3e3f3a0a49cbe
Shilajit <shilajit2014paul@gmail.com>
2023-01-04 20:42:07 +0530

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,42 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: 'home',
+  //   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  // },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
   },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  // {
+  //   path: 'tab1',
+  //   loadChildren: () => import('./tab1/tab1.module').then( m => m.Tab1PageModule)
+  // },
+  // {
+  //   path: 'tab2',
+  //   loadChildren: () => import('./tab2/tab2.module').then( m => m.Tab2PageModule)
+  // },
+  // {
+  //   path: 'tab3',
+  //   loadChildren: () => import('./tab3/tab3.module').then( m => m.Tab3PageModule)
+  // },
+  // {
+  //   path: 'tab4',
+  //   loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
+  // },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'registration',
+    loadChildren: () => import('./registration/registration.module').then( m => m.RegistrationPageModule)
   },
 ];
 
